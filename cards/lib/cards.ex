7eb482cf2a1dd636698ba45b423fe138a1d904a0
stephen grider <ste.grider@gmail.com>
2016-07-14 13:53:36 -0700

    flattened list of cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,10 +3,8 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for value <- values do
-      for suit <- suits do
-        "#{value} of #{suit}"
-      end
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
     end
   end
 
