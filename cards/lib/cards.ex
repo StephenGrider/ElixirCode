ce86ae2c27b61520e2c287b112b29985c4da96b8
stephen grider <ste.grider@gmail.com>
2016-10-17 16:46:06 -0700

    added case statement

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -24,4 +24,13 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "That file does not exist"
+    end
+  end
 end
