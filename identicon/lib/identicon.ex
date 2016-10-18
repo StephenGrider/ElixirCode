99db423b6f8f3b18107256c648f67a5294e7d34d
stephen grider <ste.grider@gmail.com>
2016-10-18 16:10:07 -0700

    pattern match from the struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,11 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    [r, g, b]
   end
 
   def hash_input(input) do
