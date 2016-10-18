e48d104af84b8ad81e48243570d5cfa9e5f59807
stephen grider <ste.grider@gmail.com>
2016-10-18 10:54:15 -0700

    added pipe operator

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -31,4 +31,10 @@ defmodule Cards do
       {:error, _reason} -> "That file does not exist"
     end
   end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
