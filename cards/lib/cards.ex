f4bc1fcdd031f2535c08b7d9d2d309e7574feac3
stephen grider <ste.grider@gmail.com>
2016-10-18 12:00:54 -0700

    added another doctest

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -19,6 +19,16 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
