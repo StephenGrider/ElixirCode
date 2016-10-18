edc3d4ac99b70fdfe58987a7be5877c9e44e3197
stephen grider <ste.grider@gmail.com>
2016-10-18 12:21:19 -0700

    added case tests

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,4 +1,14 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
 end
