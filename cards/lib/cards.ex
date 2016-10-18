676586d7cd32957ab5f6e0ddb28b80922bbc782d
stephen grider <ste.grider@gmail.com>
2016-10-18 10:43:03 -0700

    updated case

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -26,11 +26,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term binary
-      :error -> "That file does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
     end
   end
 end
