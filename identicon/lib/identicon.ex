32fe845bf4ba846249f3661e3d346006f6333f95
stephen grider <ste.grider@gmail.com>
2016-10-18 16:32:46 -0700

    updated struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,10 +5,8 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-
-    [r, g, b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -1,3 +1,3 @@
 defmodule Identicon.Image do
-  defstruct hex: nil
+  defstruct hex: nil, color: nil
 end
