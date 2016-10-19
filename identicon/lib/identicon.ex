a0580199faa698ffe817e2b5ddffbb1c3bdb9cd3
stephen grider <ste.grider@gmail.com>
2016-10-19 15:07:43 -0700

    finished grid building

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,15 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
