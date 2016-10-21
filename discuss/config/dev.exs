77c4feff4b8192ca9026ac0de5b1b9c15f1e3304
stephen grider <ste.grider@gmail.com>
2016-10-21 15:45:32 -0700

    configured database

--- discuss/config/dev.exs
+++ discuss/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "stephengrider",
+  password: "",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
