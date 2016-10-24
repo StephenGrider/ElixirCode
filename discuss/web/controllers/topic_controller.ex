4b8fa1a6c7aee0c03c9e21ea1cbf4c56b74aaa9b
stephen grider <ste.grider@gmail.com>
2016-10-24 12:00:58 -0700

    receive conn argument

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,5 +1,11 @@
 defmodule Discuss.TopicController do
-  def new() do
+  use Discuss.Web, :controller
 
+  def new(conn, params) do
+    IO.puts "++++"
+    IO.inspect conn
+    IO.puts "++++"
+    IO.inspect params
+    IO.puts "++++"
   end
 end
