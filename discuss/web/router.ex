6df926e81d105a36475da5fceaec56002dc39212
stephen grider <ste.grider@gmail.com>
2016-10-24 11:24:37 -0700

    added new route

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,6 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/topics/new", TopicController, :new
   end
 
   # Other scopes may use custom stacks.
