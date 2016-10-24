ad606cefc40bad440828a7198fc13ed7c4ef948a
stephen grider <ste.grider@gmail.com>
2016-10-24 12:17:23 -0700

    delete log

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,10 +2,6 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   def new(conn, params) do
-    IO.puts "++++"
-    IO.inspect conn
-    IO.puts "++++"
-    IO.inspect params
-    IO.puts "++++"
+
   end
 end
