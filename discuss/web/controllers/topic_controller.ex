625aa60306329a0fa614f86608392e5abb0ba0fc
stephen grider <ste.grider@gmail.com>
2016-10-25 12:52:14 -0700

    added link tag

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -35,7 +35,8 @@ defmodule Discuss.TopicController do
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
-    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
 
     case Repo.update(changeset) do
       {:ok, _topic} ->
@@ -43,7 +44,7 @@ defmodule Discuss.TopicController do
         |> put_flash(:info, "Topic Updated")
         |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
-        render conn, "edit.html", changeset: changeset
+        render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
 end
