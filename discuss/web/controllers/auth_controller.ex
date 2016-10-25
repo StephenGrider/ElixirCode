ff02e9170116ba98773503ee57dad1501a10c7e0
stephen grider <ste.grider@gmail.com>
2016-10-25 16:47:09 -0700

    made user changeset

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,7 +2,10 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
+  alias Discuss.User
+
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
   end
 end
