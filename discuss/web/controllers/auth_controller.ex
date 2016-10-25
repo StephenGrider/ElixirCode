20c8280499bb3856f54f08ce92e7aa72559c79eb
stephen grider <ste.grider@gmail.com>
2016-10-25 15:54:52 -0700

    added user model

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,7 +2,7 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    
   end
 end
---
+++ discuss/web/models/user.ex
@@ -0,0 +1,17 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token])
+    |> validate_required([:email, :provider, :token])
+  end
+end
