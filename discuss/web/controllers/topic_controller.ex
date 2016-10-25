df6ca1eb79bf63bdac51d4ada2d86fc7bedb2ba1
stephen grider <ste.grider@gmail.com>
2016-10-25 12:15:13 -0700

    added edit form

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -28,6 +28,9 @@ defmodule Discuss.TopicController do
   end
 
   def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
 
+    render conn, "edit.html", changeset: changeset, topic: topic
   end
 end
