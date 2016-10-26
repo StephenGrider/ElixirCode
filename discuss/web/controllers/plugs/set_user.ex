acf7b84028105546ef9cccdc4618864270b8e95d
stephen grider <ste.grider@gmail.com>
2016-10-25 17:47:13 -0700

    use a module plug to set current user

--- discuss/web/controllers/plugs/set_user.ex
+++ discuss/web/controllers/plugs/set_user.ex
@@ -4,12 +4,18 @@ defmodule Discuss.Plugs.SetUser do
 
   alias Discuss.Repo
   alias Discuss.User
-  alias Discuss.Router.Helpers
 
   def init(_params) do
   end
 
   def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
 
+    cond do
+      user = user_id && Repo.get(User, user_id) ->
+        assign(conn, :user, user)
+      true ->
+        assign(conn, :user, nil)
+    end
   end
 end
