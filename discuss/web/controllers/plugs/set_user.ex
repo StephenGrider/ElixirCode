c650a857bdfbd15d67026c59402e4ab32198323a
stephen grider <ste.grider@gmail.com>
2016-10-25 17:34:43 -0700

    added setuser plug

---
+++ discuss/web/controllers/plugs/set_user.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+  alias Discuss.Router.Helpers
+
+  def init(_params) do
+  end
+
+  def call(conn, _params) do
+
+  end
+end
