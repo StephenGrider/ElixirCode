3ab4afd62b85f2fde265ac5b1c92ffa033af8697
stephen grider <ste.grider@gmail.com>
2016-10-25 17:18:10 -0700

    successfully auth user

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -12,7 +12,17 @@ defmodule Discuss.AuthController do
   end
 
   defp signin(conn, changeset) do
-
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
