30e4ccdc589da55bd9f2cb77768bf50eb5316e95
stephen grider <ste.grider@gmail.com>
2016-10-27 12:46:30 -0700

    updated topic and user

--- discuss/web/models/topic.ex
+++ discuss/web/models/topic.ex
@@ -3,6 +3,7 @@ defmodule Discuss.Topic do
 
   schema "topics" do
     field :title, :string
+    belongs_to :user, Discuss.User
   end
 
   def changeset(struct, params \\ %{}) do
--- discuss/web/models/user.ex
+++ discuss/web/models/user.ex
@@ -5,6 +5,7 @@ defmodule Discuss.User do
     field :email, :string
     field :provider, :string
     field :token, :string
+    has_many :topics, Discuss.Topic
 
     timestamps()
   end
