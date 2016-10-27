ed9520670f7553c6bc160826cfb7c62a4dbc1528
stephen grider <ste.grider@gmail.com>
2016-10-27 12:19:35 -0700

    finished migration

--- discuss/priv/repo/migrations/20161027191357_add_user_id_to_topics.exs
+++ discuss/priv/repo/migrations/20161027191357_add_user_id_to_topics.exs
@@ -2,6 +2,8 @@ defmodule Discuss.Repo.Migrations.AddUserIdToTopics do
   use Ecto.Migration
 
   def change do
-
+    alter table(:topics) do
+      add :user_id, references(:users)
+    end
   end
 end
