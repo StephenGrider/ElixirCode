445cf4c08d0ddcbea4b3439cd431303160424bc4
stephengrider <ste.grider@gmail.com>
2017-09-11 10:24:45 -0700

    comment list

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -1,19 +1,36 @@
-import {Socket} from "phoenix"
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+import { Socket } from 'phoenix';
+let socket = new Socket('/socket', { params: { token: window.userToken } });
 
-socket.connect()
+socket.connect();
 
-const createSocket = (topicId) => {
-  let channel = socket.channel(`comments:${topicId}`, {})
-  channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = topicId => {
+  let channel = socket.channel(`comments:${topicId}`, {});
+  channel
+    .join()
+    .receive('ok', resp => {
+      renderComments(resp.comments);
+    })
+    .receive('error', resp => {
+      console.log('Unable to join', resp);
+    });
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
 
     channel.push('comment:add', { content: content });
   });
+};
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+    `;
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
 }
 
 window.createSocket = createSocket;
