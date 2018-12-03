c2f941da3af500ba7b8cf61c8f0fef52c54ead24
OneNeptune <niCmXLEnGH@protonmail.com>
2018-12-02 21:18:37 -0500

    Rename deck variable as _deck
    Resolve warning while testing:
    
    ```
    warning: variable "deck" is unused
      (for doctest at) lib/cards.ex:44
    ```

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -41,7 +41,7 @@ defmodule Cards do
   ## Examples
 
       iex> deck = Cards.create_deck
-      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> {hand, _deck} = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
 
